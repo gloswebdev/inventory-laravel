1c26c40b42fbb80544906998d3474e74eea5c138
Sagar Khandar <85117554+gloswebdev@users.noreply.github.com>
2026-07-19 12:38:47 +0530

    Create CostingBomPackingMaterial.php

---
+++ app/Models/CostingBomPackingMaterial.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CostingBomPackingMaterial extends Model
+{
+    use HasFactory;
+
+    protected $table = 'costing_bom_packing_materials';
+
+    protected $fillable = [
+        'costing_bom_id',
+        'pricelist_id',
+        'raw_material_id',
+        'quantity',
+    ];
+
+    public function costingBom(): BelongsTo
+    {
+        return $this->belongsTo(CostingBom::class, 'costing_bom_id');
+    }
+
+    public function pricelist(): BelongsTo
+    {
+        return $this->belongsTo(Pricelist::class, 'pricelist_id');
+    }
+
+    public function rawMaterial(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'raw_material_id');
+    }
+}
