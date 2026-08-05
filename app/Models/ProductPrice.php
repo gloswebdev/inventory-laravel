489f4e547ce75aabfdfbf274dcf57f1e3920162e
Sagar Khandar <85117554+gloswebdev@users.noreply.github.com>
2026-08-05 14:38:28 +0530

    Update ProductPrice.php

--- app/Models/ProductPrice.php
+++ app/Models/ProductPrice.php
@@ -37,7 +37,7 @@ class ProductPrice extends Model
             ->unique('item_code')
             ->pluck('case_rate', 'item_code')
             ->toArray();
-        return array_merge($prPrices, $localPrices);
+        return array_merge($localPrices, $prPrices);
     }
 
     /**
